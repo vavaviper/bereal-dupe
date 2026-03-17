892ed346f64d4028a2cebd143085aed55a32a0c3
Varsha Prasad <varshaprasad0601@gmail.com>
2026-03-16 20:41:52 -0400

    firebase update

--- server/firebase.js
+++ server/firebase.js
@@ -1,11 +1,25 @@
 const admin = require("firebase-admin");
+const fs = require("fs");
 const path = require("path");
 
-const serviceAccount = require(path.join(__dirname, "serviceAccountKey.json"));
+const credPath =
+  process.env.FIREBASE_SERVICE_ACCOUNT_PATH ||
+  process.env.GOOGLE_APPLICATION_CREDENTIALS;
+const resolved = credPath
+  ? path.isAbsolute(credPath)
+    ? credPath
+    : path.resolve(process.cwd(), credPath)
+  : path.join(__dirname, "bereal-dupe-firebase-adminsdk-fbsvc-d134b29a7e.json");
+
+if (!fs.existsSync(resolved)) {
+  throw new Error(`Firebase service account key not found at ${resolved}. Set FIREBASE_SERVICE_ACCOUNT_PATH in .env`);
+}
+
+const serviceAccount = JSON.parse(fs.readFileSync(resolved, "utf8"));
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://bereal-dupe-default-rtdb.firebaseio.com",
+  databaseURL: process.env.FIREBASE_DATABASE_URL || "https://bereal-dupe-default-rtdb.firebaseio.com",
 });
 
 const db = admin.database();
